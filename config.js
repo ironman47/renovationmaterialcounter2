bca759677e94e56607632626ca1c38ea82440581
ironman47 <ironblue80@gmail.com>
2019-08-02 23:05:13 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+//change file name to "config.js"
+module.exports = {
+  botToken: '971993274:AAGxwjMalgAsXauAdMb2QGOt0MlxtlWtqsI',
+  adminChatId: 971993274,       //your telegram chat id here, allows usage of /broadcast command
+}
